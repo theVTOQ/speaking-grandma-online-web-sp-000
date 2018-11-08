e5c7f0d4d0fa4e4058472fabf68cef9e779686a7
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-08 19:53:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,14 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(greeting)
+  lowercase = greeting.match(/[a-z]/)
+  if lowercase != nil
+    puts "HUH! SPEAK UP, SONNY!"
+  elsif greeting == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "NO, NOT SINCE 1938!"
+  end
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
